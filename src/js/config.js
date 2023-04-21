1c2424c3fb04c26e42e30d74aa98b3c569954db3
nakaobie <refinerydrive36@gmail.com>
2023-04-21 14:24:23 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '993f3f3b-b68e-4be4-84a1-791f9874d7f7';
+export const MODAL_CLOSE_SEC = 2.5;
